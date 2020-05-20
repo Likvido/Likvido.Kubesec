2b7d1d6e0af9c8da8ab73637ebb17557a98966e4
Søren Pedersen <sh.pedersen@gmail.com>
2020-05-20 16:48:20 +0200

    Add KubeCtl class to wrap calling the kubectl tool

---
+++ Likvido.Confocto/Likvido.Confocto/KubeCtl.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Likvido.Confocto
+{
+    public static class KubeCtl
+    {
+        public static Dictionary<string, string> GetSecrets(string context, string secretsName)
+        {
+            var rawSecrets = ExecuteCommand($"get secret {secretsName} -o json");
+            dynamic deserialized = JsonConvert.DeserializeObject(rawSecrets);
+
+            var secrets = new Dictionary<string, string>();
+
+            foreach (var secret in deserialized.data.Children())
+            {
+                secrets.Add(secret.Name, Encoding.UTF8.GetString(Convert.FromBase64String(secret.Value.Value)));
+            }
+
+            return secrets;
+        }
+
+        private static string ExecuteCommand(string command)
+        {
+            using var kubectl = new Process();
+            kubectl.StartInfo.FileName = "kubectl";
+            kubectl.StartInfo.Arguments = command;
+
+            kubectl.StartInfo.UseShellExecute = false;
+            kubectl.StartInfo.CreateNoWindow = true;
+            kubectl.StartInfo.RedirectStandardOutput = true;
+
+            kubectl.Start();
+            kubectl.WaitForExit();
+
+            return kubectl.StandardOutput.ReadToEnd();
+        }
+    }
+}
