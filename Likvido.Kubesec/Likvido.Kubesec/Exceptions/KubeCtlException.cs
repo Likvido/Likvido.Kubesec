d323c9fcd72f757432d0d678bda9e16a6b9c7efe
Søren Pedersen <sh.pedersen@gmail.com>
2020-06-03 18:24:00 +0200

    Add exception: KubeCtlException

---
+++ Likvido.Kubesec/Likvido.Kubesec/Exceptions/KubeCtlException.cs
@@ -0,0 +1,12 @@
+﻿namespace Likvido.Kubesec.Exceptions
+{
+    using System;
+
+    public class KubeCtlException : Exception
+    {
+        public KubeCtlException(string message)
+            : base(message)
+        {
+        }
+    }
+}
